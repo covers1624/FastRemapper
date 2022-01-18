0279da41ccd595139b7cbdc45e2e4c03682f4135
covers1624 <laughlan.cov@internode.on.net>
2022-01-18 20:04:13 +1030

    Cleanup license header noise

--- src/main/java/net/covers1624/fastremap/ASMClassRemapper.java
+++ src/main/java/net/covers1624/fastremap/ASMClassRemapper.java
@@ -1,8 +1,3 @@
-/*
- * This file is part of Fast Remapper and is Licensed under the MIT License.
- *
- * Copyright (c) 2018-2021 covers1624 <https://github.com/covers1624>
- */
 package net.covers1624.fastremap;
 
 import org.objectweb.asm.*;
