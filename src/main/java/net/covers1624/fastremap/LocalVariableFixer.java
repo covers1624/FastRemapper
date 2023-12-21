fc41ebe39b51135989d8be0c6abbe38f9420fa7e
covers1624 <covers1624@gmail.com>
2023-12-22 02:50:24 +1030

    Propagates lambda captured var parameter names in LocalVariableFixer.

--- src/main/java/net/covers1624/fastremap/LocalVariableFixer.java
+++ src/main/java/net/covers1624/fastremap/LocalVariableFixer.java
@@ -2,7 +2,9 @@ package net.covers1624.fastremap;
 
 import org.objectweb.asm.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -11,7 +13,7 @@ import java.util.Map;
 public final class LocalVariableFixer extends ClassVisitor {
 
     // Lambda target method -> Outer owning method
-    private final Map<String, String> lambdaMap = new HashMap<>();
+    private final Map<String, OuterLambdaScope> lambdaMap = new HashMap<>();
 
     private final FastRemapper remapper;
 
@@ -42,9 +44,9 @@ public final class LocalVariableFixer extends ClassVisitor {
         int paramWidth = getParamWidth(desc);
         boolean isStatic = (access & Opcodes.ACC_STATIC) != 0;
         int parentDepth;
-        String lambda = lambdaMap.get(name + desc);
+        OuterLambdaScope lambda = lambdaMap.get(name + desc);
         if (lambda != null && (access & Opcodes.ACC_SYNTHETIC) != 0) {
-            parentDepth = remapper.getMethodDepth(cName, lambda);
+            parentDepth = remapper.getMethodDepth(cName, lambda.method);
         } else if (outerMethodDepth != null) {
             parentDepth = outerMethodDepth;
         } else {
@@ -54,6 +56,8 @@ public final class LocalVariableFixer extends ClassVisitor {
         String mDesc = desc;
         return new MethodVisitor(Opcodes.ASM9, super.visitMethod(access, name, desc, signature, exceptions)) {
 
+            private final List<String> localNames = new ArrayList<>();
+
             public String uniqueLocal(String name) {
                 if (parentDepth != 0) {
                     if (parentDepth == 1) return "l_" + name;
@@ -62,17 +66,31 @@ public final class LocalVariableFixer extends ClassVisitor {
                 return name;
             }
 
+            private String nameLocal(int index) {
+                int instanceOffset = isStatic ? 0 : 1;
+                if (index == 0 && !isStatic) return "this";
+                // If we are inside a lambda, we can use captured scope vars.
+                //  This list will contain a 'this' param at index 0 for instance lambdas.
+                //  This is ignored as per the above if block.
+                //  This relies on the fact that lambdas have all captured variables declared first,
+                //  prior to any parameters from their interface implementation.
+                if (lambda != null && lambda.scopeVars.size() > index) return lambda.scopeVars.get(index);
+                if (index < paramWidth + instanceOffset) return uniqueLocal("param" + (index - instanceOffset));
+                return uniqueLocal("var" + index);
+            }
+
             @Override
             public void visitLocalVariable(String name, String descriptor, String signature, Label start, Label end, int index) {
-                if (index == 0 && !isStatic) {
-                    name = "this";
-                } else if (index < (isStatic ? paramWidth : paramWidth + 1)) {
-                    name = uniqueLocal("param" + (index - (isStatic ? 0 : 1)));
-                } else {
-                    name = uniqueLocal("var" + index);
-                }
+                super.visitLocalVariable(nameLocal(index), descriptor, signature, start, end, index);
+            }
 
-                super.visitLocalVariable(name, descriptor, signature, start, end, index);
+            @Override
+            public void visitVarInsn(int opcode, int var) {
+                if (opcode >= Opcodes.ILOAD && opcode <= Opcodes.ALOAD) {
+                    // Push all local variable load names into this list.
+                    localNames.add(nameLocal(var));
+                }
+                super.visitVarInsn(opcode, var);
             }
 
             @Override
@@ -83,7 +101,12 @@ public final class LocalVariableFixer extends ClassVisitor {
                     if (targetHandle.getOwner().equals(cName)) {
                         String tName = targetHandle.getName();
                         String tDesc = targetHandle.getDesc();
-                        lambdaMap.put(tName + tDesc, mName + mDesc);
+                        // The bytecode stack descriptor for the indy will contain the number of arguments bound to the lambda.
+                        //  One of these will be a 'ALOAD 0' (this) for instance lambdas, we don't bother stripping that here.
+                        //  Lambda indy will always load captured vars onto the stack from locals prior to the indy call.
+                        int nArgs = Type.getArgumentTypes(descriptor).length;
+                        List<String> vars = localNames.subList(localNames.size() - nArgs, localNames.size());
+                        lambdaMap.put(tName + tDesc, new OuterLambdaScope(mName + mDesc, List.copyOf(vars)));
                     }
                 }
             }
@@ -103,4 +126,6 @@ public final class LocalVariableFixer extends ClassVisitor {
         }
         return width;
     }
+
+    private record OuterLambdaScope(String method, List<String> scopeVars) { }
 }
--- src/test/java/net/covers1624/fastremap/LocalVariableFixerTests.java
+++ src/test/java/net/covers1624/fastremap/LocalVariableFixerTests.java
@@ -66,4 +66,241 @@ public class LocalVariableFixerTests extends TestBase {
                 transform(TestLocalVariableLambdaCollision.class, LOCALS_ONLY, FLAGS)
         );
     }
+
+    private static class TestLambdaLocalVariableNameCapture {
+
+        public void simple(String a) {
+            List.of().forEach(e -> {
+                System.out.println(a);
+            });
+        }
+
+        public void multipleVars(String a, String b) {
+            List.of().forEach(e -> {
+                System.out.println(a);
+                System.out.println(b);
+            });
+        }
+
+        public void nonStaticLambda(String a) {
+            List.of().forEach(e -> {
+                System.out.println(a);
+                System.out.println(this);
+            });
+        }
+
+        public void nestedLambda(String a, String b) {
+            List.of().forEach(e -> {
+                System.out.println(a);
+                List.of().forEach(e2 -> {
+                    System.out.println(b);
+                });
+            });
+        }
+    }
+
+    @Test
+    public void testLambdaLocalVariableNameCapture() {
+        assertEquals("""
+                        // class version 61.0 (61)
+                        // access flags 0x20
+                        class net/covers1624/fastremap/LocalVariableFixerTests$TestLambdaLocalVariableNameCapture {
+
+
+                          // access flags 0x1
+                          public simple(Ljava/lang/String;)V
+                           L0
+                            INVOKESTATIC java/util/List.of ()Ljava/util/List; (itf)
+                            ALOAD 1
+                            INVOKEDYNAMIC accept(Ljava/lang/String;)Ljava/util/function/Consumer; [
+                              // handle kind 0x6 : INVOKESTATIC
+                              java/lang/invoke/LambdaMetafactory.metafactory(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodHandle;Ljava/lang/invoke/MethodType;)Ljava/lang/invoke/CallSite;
+                              // arguments:
+                              (Ljava/lang/Object;)V,\s
+                              // handle kind 0x6 : INVOKESTATIC
+                              net/covers1624/fastremap/LocalVariableFixerTests$TestLambdaLocalVariableNameCapture.lambda$simple$0(Ljava/lang/String;Ljava/lang/Object;)V,\s
+                              (Ljava/lang/Object;)V
+                            ]
+                            INVOKEINTERFACE java/util/List.forEach (Ljava/util/function/Consumer;)V (itf)
+                           L1
+                            RETURN
+                           L2
+                            LOCALVARIABLE this Lnet/covers1624/fastremap/LocalVariableFixerTests$TestLambdaLocalVariableNameCapture; L0 L2 0
+                            LOCALVARIABLE param0 Ljava/lang/String; L0 L2 1
+                            MAXSTACK = 2
+                            MAXLOCALS = 2
+
+                          // access flags 0x1
+                          public multipleVars(Ljava/lang/String;Ljava/lang/String;)V
+                           L0
+                            INVOKESTATIC java/util/List.of ()Ljava/util/List; (itf)
+                            ALOAD 1
+                            ALOAD 2
+                            INVOKEDYNAMIC accept(Ljava/lang/String;Ljava/lang/String;)Ljava/util/function/Consumer; [
+                              // handle kind 0x6 : INVOKESTATIC
+                              java/lang/invoke/LambdaMetafactory.metafactory(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodHandle;Ljava/lang/invoke/MethodType;)Ljava/lang/invoke/CallSite;
+                              // arguments:
+                              (Ljava/lang/Object;)V,\s
+                              // handle kind 0x6 : INVOKESTATIC
+                              net/covers1624/fastremap/LocalVariableFixerTests$TestLambdaLocalVariableNameCapture.lambda$multipleVars$1(Ljava/lang/String;Ljava/lang/String;Ljava/lang/Object;)V,\s
+                              (Ljava/lang/Object;)V
+                            ]
+                            INVOKEINTERFACE java/util/List.forEach (Ljava/util/function/Consumer;)V (itf)
+                           L1
+                            RETURN
+                           L2
+                            LOCALVARIABLE this Lnet/covers1624/fastremap/LocalVariableFixerTests$TestLambdaLocalVariableNameCapture; L0 L2 0
+                            LOCALVARIABLE param0 Ljava/lang/String; L0 L2 1
+                            LOCALVARIABLE param1 Ljava/lang/String; L0 L2 2
+                            MAXSTACK = 3
+                            MAXLOCALS = 3
+
+                          // access flags 0x1
+                          public nonStaticLambda(Ljava/lang/String;)V
+                           L0
+                            INVOKESTATIC java/util/List.of ()Ljava/util/List; (itf)
+                            ALOAD 0
+                            ALOAD 1
+                            INVOKEDYNAMIC accept(Lnet/covers1624/fastremap/LocalVariableFixerTests$TestLambdaLocalVariableNameCapture;Ljava/lang/String;)Ljava/util/function/Consumer; [
+                              // handle kind 0x6 : INVOKESTATIC
+                              java/lang/invoke/LambdaMetafactory.metafactory(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodHandle;Ljava/lang/invoke/MethodType;)Ljava/lang/invoke/CallSite;
+                              // arguments:
+                              (Ljava/lang/Object;)V,\s
+                              // handle kind 0x5 : INVOKEVIRTUAL
+                              net/covers1624/fastremap/LocalVariableFixerTests$TestLambdaLocalVariableNameCapture.lambda$nonStaticLambda$2(Ljava/lang/String;Ljava/lang/Object;)V,\s
+                              (Ljava/lang/Object;)V
+                            ]
+                            INVOKEINTERFACE java/util/List.forEach (Ljava/util/function/Consumer;)V (itf)
+                           L1
+                            RETURN
+                           L2
+                            LOCALVARIABLE this Lnet/covers1624/fastremap/LocalVariableFixerTests$TestLambdaLocalVariableNameCapture; L0 L2 0
+                            LOCALVARIABLE param0 Ljava/lang/String; L0 L2 1
+                            MAXSTACK = 3
+                            MAXLOCALS = 2
+
+                          // access flags 0x1
+                          public nestedLambda(Ljava/lang/String;Ljava/lang/String;)V
+                           L0
+                            INVOKESTATIC java/util/List.of ()Ljava/util/List; (itf)
+                            ALOAD 1
+                            ALOAD 2
+                            INVOKEDYNAMIC accept(Ljava/lang/String;Ljava/lang/String;)Ljava/util/function/Consumer; [
+                              // handle kind 0x6 : INVOKESTATIC
+                              java/lang/invoke/LambdaMetafactory.metafactory(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodHandle;Ljava/lang/invoke/MethodType;)Ljava/lang/invoke/CallSite;
+                              // arguments:
+                              (Ljava/lang/Object;)V,\s
+                              // handle kind 0x6 : INVOKESTATIC
+                              net/covers1624/fastremap/LocalVariableFixerTests$TestLambdaLocalVariableNameCapture.lambda$nestedLambda$4(Ljava/lang/String;Ljava/lang/String;Ljava/lang/Object;)V,\s
+                              (Ljava/lang/Object;)V
+                            ]
+                            INVOKEINTERFACE java/util/List.forEach (Ljava/util/function/Consumer;)V (itf)
+                           L1
+                            RETURN
+                           L2
+                            LOCALVARIABLE this Lnet/covers1624/fastremap/LocalVariableFixerTests$TestLambdaLocalVariableNameCapture; L0 L2 0
+                            LOCALVARIABLE param0 Ljava/lang/String; L0 L2 1
+                            LOCALVARIABLE param1 Ljava/lang/String; L0 L2 2
+                            MAXSTACK = 3
+                            MAXLOCALS = 3
+
+                          // access flags 0x100A
+                          private static synthetic lambda$nestedLambda$4(Ljava/lang/String;Ljava/lang/String;Ljava/lang/Object;)V
+                           L0
+                            GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+                            ALOAD 0
+                            INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
+                           L1
+                            INVOKESTATIC java/util/List.of ()Ljava/util/List; (itf)
+                            ALOAD 1
+                            INVOKEDYNAMIC accept(Ljava/lang/String;)Ljava/util/function/Consumer; [
+                              // handle kind 0x6 : INVOKESTATIC
+                              java/lang/invoke/LambdaMetafactory.metafactory(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodHandle;Ljava/lang/invoke/MethodType;)Ljava/lang/invoke/CallSite;
+                              // arguments:
+                              (Ljava/lang/Object;)V,\s
+                              // handle kind 0x6 : INVOKESTATIC
+                              net/covers1624/fastremap/LocalVariableFixerTests$TestLambdaLocalVariableNameCapture.lambda$nestedLambda$3(Ljava/lang/String;Ljava/lang/Object;)V,\s
+                              (Ljava/lang/Object;)V
+                            ]
+                            INVOKEINTERFACE java/util/List.forEach (Ljava/util/function/Consumer;)V (itf)
+                           L2
+                            RETURN
+                           L3
+                            LOCALVARIABLE param0 Ljava/lang/String; L0 L3 0
+                            LOCALVARIABLE param1 Ljava/lang/String; L0 L3 1
+                            LOCALVARIABLE l_param2 Ljava/lang/Object; L0 L3 2
+                            MAXSTACK = 2
+                            MAXLOCALS = 3
+
+                          // access flags 0x100A
+                          private static synthetic lambda$nestedLambda$3(Ljava/lang/String;Ljava/lang/Object;)V
+                           L0
+                            GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+                            ALOAD 0
+                            INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
+                           L1
+                            RETURN
+                           L2
+                            LOCALVARIABLE param1 Ljava/lang/String; L0 L2 0
+                            LOCALVARIABLE l2_param1 Ljava/lang/Object; L0 L2 1
+                            MAXSTACK = 2
+                            MAXLOCALS = 2
+
+                          // access flags 0x1002
+                          private synthetic lambda$nonStaticLambda$2(Ljava/lang/String;Ljava/lang/Object;)V
+                           L0
+                            GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+                            ALOAD 1
+                            INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
+                           L1
+                            GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+                            ALOAD 0
+                            INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/Object;)V
+                           L2
+                            RETURN
+                           L3
+                            LOCALVARIABLE this Lnet/covers1624/fastremap/LocalVariableFixerTests$TestLambdaLocalVariableNameCapture; L0 L3 0
+                            LOCALVARIABLE param0 Ljava/lang/String; L0 L3 1
+                            LOCALVARIABLE l_param1 Ljava/lang/Object; L0 L3 2
+                            MAXSTACK = 2
+                            MAXLOCALS = 3
+
+                          // access flags 0x100A
+                          private static synthetic lambda$multipleVars$1(Ljava/lang/String;Ljava/lang/String;Ljava/lang/Object;)V
+                           L0
+                            GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+                            ALOAD 0
+                            INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
+                           L1
+                            GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+                            ALOAD 1
+                            INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
+                           L2
+                            RETURN
+                           L3
+                            LOCALVARIABLE param0 Ljava/lang/String; L0 L3 0
+                            LOCALVARIABLE param1 Ljava/lang/String; L0 L3 1
+                            LOCALVARIABLE l_param2 Ljava/lang/Object; L0 L3 2
+                            MAXSTACK = 2
+                            MAXLOCALS = 3
+
+                          // access flags 0x100A
+                          private static synthetic lambda$simple$0(Ljava/lang/String;Ljava/lang/Object;)V
+                           L0
+                            GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+                            ALOAD 0
+                            INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
+                           L1
+                            RETURN
+                           L2
+                            LOCALVARIABLE param0 Ljava/lang/String; L0 L2 0
+                            LOCALVARIABLE l_param1 Ljava/lang/Object; L0 L2 1
+                            MAXSTACK = 2
+                            MAXLOCALS = 2
+                        }
+                        """,
+                transform(TestLambdaLocalVariableNameCapture.class, LOCALS_ONLY, FLAGS)
+        );
+    }
+
 }
